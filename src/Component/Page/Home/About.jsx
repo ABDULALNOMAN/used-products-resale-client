3a553dcc27455687056bc9548befcb7e91f506bb
unknown <nomanmd1st2001@gmail.com>
2022-11-28 23:25:47 +0600

    modify about

--- src/Component/Page/Home/About.jsx
+++ src/Component/Page/Home/About.jsx
@@ -4,12 +4,12 @@ import aboutImg from '../../../assets/nHYWTGvx2121UCI0SePNsJlUdJ62VNyQ.jpg'
 const About = () => {
     return (
         <div className='bg-teal-800 p-3 my-8'>
-            <div className='container mx-auto grid grid-cols-3'>
+            <div className='container mx-auto grid md:grid-cols-3 grid-cols-1'>
                 <div className='col-span-1'>
-                    <img src={aboutImg} alt="" />
+                    <img  className='h-full' src={aboutImg} alt="" />
                 </div>
                 <div className='col-span-2 ml-4'>
-                    <p>gsmarea is populer movile exchange application best movile phone price only gsmarea.many people use is because of the good user exprrience</p>
+                    <p className='text-lg'>gsmarea is populer movile exchange application best movile phone price only gsmarea.many people use is because of the good user exprrience</p>
                 </div>
             </div>
         </div>
